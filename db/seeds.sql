26b39b3f3f8f70a14366c3f267a4676affa5cb41
Ihab Ali <Ihab Ali>
2017-05-22 17:38:04 -0400

    added html images for index

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,15 +9,19 @@ VALUES ("Cameras", 0027242878822, "sony a 600", 'sony mirrorless DSLR with the l
 
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Cameras", 027242876965, "Sony DSC-W830 ", 'Sony DSC-W830 silver ', 10, 129);
+
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Cameras", 027242876972, "Sony DSC-W830 ", 'Sony DSC-W830 black ', 10, 129);
+
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Cameras", 4905524972498, "Sony DSC-W830 ", 'Sony DSC-W830 purple ', 8, 149);
+
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Cameras", 027242877115, "Sony DSC-W00 ", 'Sony DSC-W00 black ', 5, 189);
 
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Laptops", 888462687706, "Apple MacBook MLHE2LL/A  ", 'Apple MacBook MLHE2LL/A 12-Inch Laptop with Retina Display', 3, 1489);
+
 INSERT INTO products (category, upc, product_name, product_desc, quantity, price)
 VALUES ("Laptops", 888462664509, "Apple MacBook Pro MLH32LL/A", 'Apple MacBook Pro MLH32LL/A 15.4-inch Laptop with Touch Bar', 5, 2489);
 
